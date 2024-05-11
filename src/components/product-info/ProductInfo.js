9e1d1ef91b7a090521f484500b56d19c24bb204c
SamikQ <samokhvalov.dmitriy@gmail.com>
2024-05-12 00:42:18 +0300

    product page

--- src/components/product-info/ProductInfo.js
+++ src/components/product-info/ProductInfo.js
@@ -1,12 +1,15 @@
 import { useState, useEffect } from "react";
 
-import decor from "../../resources/img/decor/decor-golden-round.png";
 import heart from "../../resources/img/client-icons/heart.svg";
 import info from "../../resources/img/client-icons/information.svg";
 import Slider from "../slider/Slider";
 import sizeTable from "../../resources/img/size-table.webp";
 
 import temp from "../../resources/img/temp/cart-item.png";
+import preview1 from "../../resources/img/temp/look-temp.jpg";
+import preview2 from "../../resources/img/temp/look-temp2.jpg";
+import preview3 from "../../resources/img/temp/look-temp3.jpg";
+import preview4 from "../../resources/img/temp/look-temp4.jpg";
 
 const ProductInfo = () => {
     const [screenWidth, setScreenWidth] = useState(window.innerWidth);
@@ -20,12 +23,6 @@ const ProductInfo = () => {
         return (
             <section className="product__page" title="інформація по продукту">
                 <div className="wrapper">
-                    <div className="navigation-list">
-                        {/* <div className="navigation-list-decor">
-                            <img src={decor} alt="decorated golden round" />
-                        </div> */}
-                        {/* <span>Home / Колекції / Eternal Elegance</span> */}
-                    </div>
                     <div className="product__information">
                         <div className="product__slider">
                             <img src={temp} alt="will be removed" />
@@ -189,142 +186,155 @@ const ProductInfo = () => {
         return (
             <section className="product__page" title="інформація по продукту">
                 <div className="wrapper">
-                    <div className="navigation-list">
-                        <div className="navigation-list-decor">
-                            <img src={decor} alt="decorated golden round" />
-                        </div>
-                        <span>Home / Колекції / Eternal Elegance</span>
-                    </div>
                     <div className="product__information">
-                        <div className="product__slider">
-                            <img src={temp} alt="will be removed" />
-                        </div>
-                        <h3 className="product__information-title">
-                            Костюм базовий двійка з жакетом та довгими брюками
-                        </h3>
-                        <div className="product__information-price">
-                            <p>1 420₴</p>
-                            <img src={heart} alt="heart icon" />
-                        </div>
-                        <p className="product__information-delivery">
-                            Можливість міжнародної доставки
-                        </p>
-                        <div className="product__information-size">
-                            <div className="product__information-size-title">
-                                <p>
-                                    Розмір: <span>M</span>
-                                </p>
-                                <a href="/#" className="size-title">
-                                    <img src={info} alt="information icon" />
-                                    <p>Розмірна сітка</p>
-                                </a>
+                        <div className="product__preview">
+                            <div className="product__slider">
+                                <img src={temp} alt="will be removed" />
                             </div>
-                            <div className="product__information-size__selector">
-                                <ul className="size__selector-list">
-                                    <li className="size__selector">
-                                        <p className="size__selector-text">
-                                            XS
-                                        </p>
-                                    </li>
-                                </ul>
-                                <ul className="size__selector-list">
-                                    <li className="size__selector">
-                                        <p className="size__selector-text">S</p>
-                                    </li>
-                                </ul>
-                                <ul className="size__selector-list">
-                                    <li className="size__selector selected">
-                                        <span className="size__selector-text selected__size">
-                                            M
-                                        </span>
-                                    </li>
-                                </ul>
-                                <ul className="size__selector-list">
-                                    <li className="size__selector">
-                                        <p className="size__selector-text">L</p>
-                                    </li>
-                                </ul>
-                                <ul className="size__selector-list">
-                                    <li className="size__selector">
-                                        <p className="size__selector-text">
-                                            XL
-                                        </p>
-                                    </li>
-                                </ul>
+                            <div className="product__slider-preview">
+                                <img src={preview1} alt="" />
+                                <img src={preview2} alt="" />
+                                <img src={preview3} alt="" />
+                                <img src={preview4} alt="" />
                             </div>
                         </div>
-                        <div className="product__information-color__selector">
-                            <div className="product__information-size-title">
-                                <p>
-                                    Колір: <span>Синій</span>
-                                </p>
+                        <div className="product__information-description">
+                            <h3 className="product__information-title">
+                                Костюм базовий двійка з жакетом та довгими
+                                брюками
+                            </h3>
+                            <div className="product__information-price">
+                                <p>1 420₴</p>
+                                <img src={heart} alt="heart icon" />
                             </div>
-                            <div className="product__color-container">
-                                <div className="product__colors">
-                                    <span
-                                        className="product__color selected__color"
-                                        primary="#FAE7CD"
-                                        color="beige"
-                                        data-price="300"></span>
-
-                                    <span
-                                        className="product__color"
-                                        primary="#848282"
-                                        color="grey"
-                                        data-price="570"></span>
-                                    <span
-                                        className="product__color"
-                                        primary="#01337A"
-                                        color="blueBright"
-                                        data-price="470"></span>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="product__purchase">
-                            <button className="btn btn-black submit-order__btn">
-                                Додати в кошик
-                            </button>
-                            <p className="product__purchase-descr">
-                                Цей товар присутній у фізичних магазинах. Ви
-                                також можете приміряти його замовивши через
-                                накладений платіж Нової Пошти. Безкоштовне
-                                повернення.
+                            <p className="product__information-delivery">
+                                Можливість міжнародної доставки
                             </p>
-                        </div>
-                        <div className="product__description">
-                            <details className="product__description-item">
-                                <summary className="product__descriptions-title">
-                                    Опис товару<div className="accordion"></div>
-                                </summary>
-                                <div className="product__description-text">
-                                    <span>
-                                        чорний жакет фантастичного кольору з
-                                        родзинками святого одноріга
-                                    </span>
+                            <div className="product__information-size">
+                                <div className="product__information-size-title">
+                                    <p>
+                                        Розмір: <span>M</span>
+                                    </p>
+                                    <a href="/#" className="size-title">
+                                        <img
+                                            src={info}
+                                            alt="information icon"
+                                        />
+                                        <p>Розмірна сітка</p>
+                                    </a>
                                 </div>
-                            </details>
-                            <details className="product__description-item">
-                                <summary className="product__descriptions-title">
-                                    Склад<div className="accordion"></div>
-                                </summary>
-                                <div className="product__description-text">
-                                    <span>
-                                        чорний жакет фантастичного кольору з
-                                        родзинками святого одноріга
-                                    </span>
+                                <div className="product__information-size__selector">
+                                    <ul className="size__selector-list">
+                                        <li className="size__selector">
+                                            <p className="size__selector-text">
+                                                XS
+                                            </p>
+                                        </li>
+                                    </ul>
+                                    <ul className="size__selector-list">
+                                        <li className="size__selector">
+                                            <p className="size__selector-text">
+                                                S
+                                            </p>
+                                        </li>
+                                    </ul>
+                                    <ul className="size__selector-list">
+                                        <li className="size__selector selected">
+                                            <span className="size__selector-text selected__size">
+                                                M
+                                            </span>
+                                        </li>
+                                    </ul>
+                                    <ul className="size__selector-list">
+                                        <li className="size__selector">
+                                            <p className="size__selector-text">
+                                                L
+                                            </p>
+                                        </li>
+                                    </ul>
+                                    <ul className="size__selector-list">
+                                        <li className="size__selector">
+                                            <p className="size__selector-text">
+                                                XL
+                                            </p>
+                                        </li>
+                                    </ul>
                                 </div>
-                            </details>
-                            <details className="product__description-item">
-                                <summary className="product__descriptions-title">
-                                    Догляд<div className="accordion"></div>
-                                </summary>
-                                <div className="product__description-text">
-                                    <span>
-                                        чорний жакет фантастичного кольору з
-                                        родзинками святого одноріга
-                                    </span>
+                            </div>
+                            <div className="product__information-color__selector">
+                                <div className="product__information-size-title">
+                                    <p>
+                                        Колір: <span>Синій</span>
+                                    </p>
                                 </div>
-                            </details>
+                                <div className="product__color-container">
+                                    <div className="product__colors">
+                                        <span
+                                            className="product__color selected__color"
+                                            primary="#FAE7CD"
+                                            color="beige"
+                                            data-price="300"></span>
+
+                                        <span
+                                            className="product__color"
+                                            primary="#848282"
+                                            color="grey"
+                                            data-price="570"></span>
+                                        <span
+                                            className="product__color"
+                                            primary="#01337A"
+                                            color="blueBright"
+                                            data-price="470"></span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="product__purchase">
+                                <button className="btn btn-black submit-order__btn">
+                                    Додати в кошик
+                                </button>
+                                <p className="product__purchase-descr">
+                                    Цей товар присутній у фізичних магазинах. Ви
+                                    також можете приміряти його замовивши через
+                                    накладений платіж Нової Пошти. Безкоштовне
+                                    повернення.
+                                </p>
+                            </div>
+                            <div className="product__description">
+                                <details className="product__description-item">
+                                    <summary className="product__descriptions-title">
+                                        Опис товару
+                                        <div className="accordion"></div>
+                                    </summary>
+                                    <div className="product__description-text">
+                                        <span>
+                                            чорний жакет фантастичного кольору з
+                                            родзинками святого одноріга
+                                        </span>
+                                    </div>
+                                </details>
+                                <details className="product__description-item">
+                                    <summary className="product__descriptions-title">
+                                        Склад<div className="accordion"></div>
+                                    </summary>
+                                    <div className="product__description-text">
+                                        <span>
+                                            чорний жакет фантастичного кольору з
+                                            родзинками святого одноріга
+                                        </span>
+                                    </div>
+                                </details>
+                                <details className="product__description-item">
+                                    <summary className="product__descriptions-title">
+                                        Догляд<div className="accordion"></div>
+                                    </summary>
+                                    <div className="product__description-text">
+                                        <span>
+                                            чорний жакет фантастичного кольору з
+                                            родзинками святого одноріга
+                                        </span>
+                                    </div>
+                                </details>
+                            </div>
                         </div>
                     </div>
                     <div className="interested">
